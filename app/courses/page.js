92302c139d2684ae00c9fb08606df9477ed13d08
jagveerChauhan <jagveer@onetick.in>
2024-04-05 22:38:46 +0530

    fixed cards padding in courses page

--- app/courses/page.js
+++ app/courses/page.js
@@ -54,7 +54,7 @@ const CourseCard = ({course}) =>{
   const {image,title, category_name, slug} = course;
   return(
     <Link className="group flex flex-col font-Poppin rounded-md bg-white justify-between hover:-translate-y-2 hover:shadow-md hover:shadow-neutral-500 transition-all" href={`/courses/${slug}`}>
-      <div className="px-4 pt-4">
+      <div className="px-2 pt-2">
         <div className="relative h-40 overflow-hidden">
           <Image className="object-cover rounded-md group-hover:scale-110 transition-all" src={image} alt={"title"} fill={true}></Image>
         </div> 
